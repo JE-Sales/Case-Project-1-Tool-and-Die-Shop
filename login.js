5c9a8bf679aa2ac6368b876545a46e11d45a8943
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2024-03-20 22:38:43 +0800

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    var userType = document.getElementById("userType").value;
-    if (userType === "user" || userType === "admin") {
-        window.location.href = "add.html"; // Replace with your add page URL
-        event.preventDefault(); // Prevent default form submission
-    }
-});
